566116afac639812df8f455d01ef2967184abd95
RyanFrazier <rfrazier716@gmail.com>
2020-01-16 19:31:56 -0500

    updated projectile motion

--- examples/projectile_motion.cpp
+++ examples/projectile_motion.cpp
@@ -39,20 +39,20 @@ std::string GlmVectorString(glm::vec4 vector)
 int main() 
 {
 	//make a new projectile instance
-	auto cannonball = new projectile;
-	cannonball->set_velocity(10, 10, 0); // initialize velocity
-	std::cout << "initial position of cannonball" << GlmVectorString(cannonball->position) << std::endl;
-	cannonball->time_tick(); // advance the timestep
+	auto cannonball = projectile();
+	cannonball.set_velocity(10, 10, 0); // initialize velocity
+	std::cout << "initial position of cannonball" << GlmVectorString(cannonball.position) << std::endl;
+	cannonball.time_tick(); // advance the timestep
 	//loop until cannonball intersects ground
 	std::cout << sizeof(glm::vec4)<<std::endl;
 	std::cout << sizeof(glm::vec4 *) << std::endl;
 	std::cout << sizeof(cannonball) << std::endl;
 	std::cout << sizeof(projectile) << std::endl;
-	while (cannonball->position.y >= 0)
+	while (cannonball.position.y >= 0)
 	{
-		cannonball->time_tick();
-		std::cout << "Step" << cannonball->timestep << "\t position" << GlmVectorString(cannonball->position) << std::endl;
+		cannonball.time_tick();
+		std::cout << "Step" << cannonball.timestep << "\t position" << GlmVectorString(cannonball.position) << std::endl;
 	}
-	std::cout << "Cannonball terminated after " << cannonball->timestep << " timesteps" << std::endl;
+	std::cout << "Cannonball terminated after " << cannonball.timestep << " timesteps" << std::endl;
 	return 0;
 }
