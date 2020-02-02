cc152b71879bdd8c00cd32ad93163d4c2e641341
RyanFrazier <rfrazier716@gmail.com>
2020-02-01 19:36:56 -0500

    added class for nurbs curves which needs to be worked on and then tested, going to use namespace "tracer" for these new curves

---
+++ include/tracer/geometry/nurbs.hpp
@@ -0,0 +1,15 @@
+/**
+* a nurbs curve that can evaluated along the range t=0:1, will automatically clamp the surface at ends
+*
+*
+*/
+namespace tracer
+{
+	class nurbc
+	{
+	private:
+		//knot vector
+		//control points as std::vector
+		//degree
+	};
+}
